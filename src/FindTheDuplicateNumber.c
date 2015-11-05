8435ff32959978df6f71b1013a362727d851e2b3
Si Yin <zyzyis@gmail.com>
2015-11-05 22:37:14 +0100

    Update FindTheDuplicateNumber.c

--- src/FindTheDuplicateNumber.c
+++ src/FindTheDuplicateNumber.c
@@ -2,18 +2,19 @@
 #define is_touched(v) ((v) >= secret && (v) % secret == 0)
 
 int findDuplicate(int* nums, int numsSize) {
-    int index = 0, i = 0, result = 0;
+    int index = 0, i = 0, j = 0, result = 0;
     for (i = 0; i < numsSize; i ++) {
         index = is_touched(nums[i]) ? nums[i] / secret : nums[i];
         if (is_touched(nums[index])) {
             result = index;
-            index = i;
+            j = i;
             break;
         } else
             nums[index] *= secret;
     }
     // untouch the array
-    for (i = 0; i < index; i ++)
-        nums[i] = is_touched(nums[i]) ? nums[i] / secret : nums[i];
+    for (i = 0; i < j; i ++)
+        if (is_touched(nums[i]))
+            nums[i] /= secret;
     return result;
 }
